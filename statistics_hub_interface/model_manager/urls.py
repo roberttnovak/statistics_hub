1d95337865f1e771fc76901cd5bfcbc67c7c4db0
roberttnovak <robertt_novak@hotmail.com>
2023-11-08 10:10:23 +0100

    modificaticion some urls

--- statistics_hub_interface/model_manager/urls.py
+++ statistics_hub_interface/model_manager/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('train_model', views.train_model, name='train_model'),
+    path('train_model/', views.train_model, name='train_model'),
     path('get_model_parameters/<str:model_type>/', views.get_model_parameters, name='get_model_parameters'),
-    path('data_source_selection', views.data_source_selection, name='data_source_selection'),
+    path('data_source_selection/', views.data_source_selection, name='data_source_selection'),
     path('upload_file/', views.upload_file, name='upload_file'),
     path('connect_to_database/', views.connect_to_database, name='connect_to_database'),
     path('', views.user_login, name='login'),
